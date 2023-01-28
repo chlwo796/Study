3cfaed82132c75bb5cbb09441a67902908d91171
chlwo796 <chlwo796@gmail.com>
2023-01-28 22:35:57 +0900

    230128 home

--- java_study/javaBasic4/src/javaBasic5/ex19/Account.java
+++ java_study/javaBasic4/src/javaBasic5/ex19/Account.java
@@ -1,53 +1,16 @@
 package javaBasic5.ex19;
 
-import java.util.Scanner;
+public class Account{
 
-public class Account {
-
-	Scanner sc = new Scanner(System.in);
 	private int balance; // 0<=balance<=1000000
 
-//	private static final int MIN_BALANCE = 0;
-//	private static final int MAX_BALANCE = 1000000;
+	protected static final int MIN_BALANCE = 0;
+	protected static final int MAX_BALANCE = 1000000;
 	private String account;
 	private String name;
-	private int money;
-	Account[] newArray;
-
-	public Account[] getNewArray() {
-		return newArray;
-	}
-
-	public void setNewArray(Account[] newArray) {
-		this.newArray = newArray;
-	}
-
-//	public void input() {
-//		Account[] newArray = new Account[3];
-//
-//		System.out.println("계좌번호 : ");
-//		setAccount(sc.nextLine());
-//		System.out.println("계좌주 :");
-//		setName(sc.nextLine());
-//		System.out.println("초기입금액 : ");
-//		setBalance(sc.nextLine());
-//		System.out.println("결과 : 계좌가 생성되었습니다.");
-//
-//		newArray[0] = getAccount();
-//		newArray[1] = getName();
-//		newArray[2] = getBalance();
-//
-//		setNewArray(newArray);
-////		for(int i = 0;i<accountArray.length;i++) {
-////			if(accountArray[i] == null) {
-////				accountArray = newArray;
-////			}else break;
-//	}
-
-	public void setBalance(int balance) {
-		this.balance = balance;
-	}
-
+	
+	int check = 0;
+	
 	public int getBalance() {
 		return balance;
 	}
@@ -68,19 +31,10 @@ public class Account {
 		this.name = name;
 	}
 
-	public int getMoney() {
-		return money;
-	}
+	public void setBalance(int balance) {
+		if (balance >= MIN_BALANCE && balance <= MAX_BALANCE) {
+			this.balance = balance;
+		}
 
-	public void setMoney(int money) {
-		this.money = money;
 	}
-
-//	public void setBalance(int balance) {
-//		if (balance >= MIN_BALANCE && balance <= MAX_BALANCE) {
-//
-//			this.balance = balance;
-//		} else { // else 는 return 안함
-//
-//		}
 }
--- java_study/javaBasic4/src/javaBasic5/ex19/BankApplication.java
+++ java_study/javaBasic4/src/javaBasic5/ex19/BankApplication.java
@@ -2,15 +2,14 @@ package javaBasic5.ex19;
 
 import java.util.Scanner;
 
-public class BankApplication {
+public class BankApplication extends Account {
 	static Scanner sc = new Scanner(System.in);
 	static Account[] accountArray = new Account[100];
 
 	public static void main(String[] args) {
-
+		boolean runMain = true;
 		// TODO Auto-generated method stub
-		boolean run = true;
-		while (run) {
+		while (runMain) {
 
 			System.out.println("-----------------------------------------------");
 			System.out.println("1. 계좌생성 | 2. 계좌목록 | 3. 예금 | 4. 출금 | 5. 종료");
@@ -24,10 +23,14 @@ public class BankApplication {
 			case "2":
 				list();
 				break;
-//			case "3":
-//			case "4":
+			case "3":
+				deposit();
+				break;
+			case "4":
+				withdraw();
+				break;
 			case "5":
-				run = false;
+				runMain = false;
 				break;
 			default:
 				System.out.println("잘못입력하셨습니다.");
@@ -40,25 +43,23 @@ public class BankApplication {
 	}
 
 	public static void input() {
-
-		int count = 0;
 		Account account = new Account();
 		System.out.println("-------");
 		System.out.println("계좌생성");
 		System.out.println("-------");
 		while (true) {
+			int count = 0;
 			System.out.println("계좌번호 : ");
 			String temp = sc.nextLine(); // 계좌번호 String 중복 검사를 위한 임시변수
-			if (accountArray[0] != null) {
-				for (int i = 0; i < accountArray.length; i++) {
-					if ((accountArray[i].getAccount()).equals(temp)) {
-						count++; // 배열 전체 검사 후 변하는 count 숫자로 조건문 컨트롤
-						System.out.println("계좌번호 중복입니다. 다시 입력해주세요.");
-						break;
-					} // end if
 
-				} // end for
-			} // end if(초기값 있을때만 실행)
+			for (int i = 0; i < accountArray.length; i++) {
+				if (accountArray[i] != null && temp.equals(accountArray[i].getAccount())) {
+					count++; // 배열 전체 검사 후 변하는 count 숫자로 조건문 컨트롤
+					System.out.println("계좌번호 중복입니다. 다시 입력해주세요.");
+					break;
+				} // end if
+			} // end for
+				// end if(초기값 있을때만 실행)
 			if (count == 0) {
 				account.setAccount(temp);
 				break;
@@ -67,9 +68,20 @@ public class BankApplication {
 
 		System.out.println("계좌주 : ");
 		account.setName(sc.nextLine());
-		System.out.println("초기입금액 : ");
-		account.setBalance(sc.nextInt());
-		sc.nextLine();
+
+		while (true) {
+			System.out.println("초기입금액 : ");
+			int numberCheck = sc.nextInt();
+			sc.nextLine();
+			if (numberCheck < Account.MIN_BALANCE) {
+				System.out.println("입력값은 0이상의 숫자입니다. 다시 입력해주세요");
+				continue;
+			} else {
+				account.setBalance(numberCheck);
+				break;
+			}
+		}
+		// setBalance에서 음수면 리턴을 안한다.
 
 		for (int i = 0; i < accountArray.length; i++) {
 
@@ -81,6 +93,7 @@ public class BankApplication {
 
 		}
 		System.out.println("계좌가 생성되었습니다.");
+
 	}
 
 	public static void list() {
@@ -92,9 +105,119 @@ public class BankApplication {
 			if (accountArray[i] == null) {
 				break;
 			}
-			System.out.printf("%-8s\t%s\t%d", accountArray[i].getAccount(), accountArray[i].getName(),
+			System.out.printf("%s\t%s\t%d\n", accountArray[i].getAccount(), accountArray[i].getName(),
 					accountArray[i].getBalance());
 		}
 
 	}
+
+	public static void deposit() {
+		System.out.println("-------");
+		System.out.println("예금");
+		System.out.println("-------");
+		boolean runDeposit = true; // 초기 계좌생성을 안했다면 main method 로 탈출
+//		예금 정수를 입력받고 getbalance 후 그 정수를 더한걸 리턴->다시 set. 안되는 경우를 생각해보자. -정수인경우 제외(Account class에서 제어중)
+		int count1 = 0;
+		for (int i = 0; i < accountArray.length; i++) {
+			if (accountArray[i] != null) {
+				count1++;
+			}
+		}
+		if (count1 == 0) {
+			System.out.println("계좌정보가 없습니다. 계좌생성 먼저 해주세요.");
+			runDeposit = false;
+		}
+		while (runDeposit) {
+			int count2 = 0;
+			System.out.println("계좌번호 :");
+			String inputAccount = sc.nextLine();
+			System.out.println("예금액");
+			int deposit = sc.nextInt();
+			sc.nextLine();
+
+			if (deposit <= 0) {
+				System.out.println("잘못된 금액입니다(10~)");
+				continue;
+			}
+
+			for (int i = 0; i < accountArray.length; i++) {
+				if (accountArray[i] != null && inputAccount.equals(accountArray[i].getAccount())) {
+					// count2 의 return값은 0과 1밖에없다. input에서 계좌중복, deposit에서 계좌가 아예없을때를 제어해줬다.
+					if ((deposit + accountArray[i].getBalance()) > Account.MAX_BALANCE) {
+						System.out.println("잔액이 범위를 초과합니다. 잔액을 확인하세요.");
+						count2++;
+						break;
+					} else {
+						accountArray[i].setBalance(deposit + accountArray[i].getBalance());
+						System.out.println("예금완료");
+						count2++;
+						break;
+					}
+				}
+
+			}
+			if (count2 == 0) {
+				System.out.println("계좌번호가 일치하지 않습니다. 다시 입력해주세요.");
+				continue;
+			}
+			break;
+
+		}
+	}
+
+	private static void withdraw() {
+//		출금메소드. 밸런스를 가져와서 -면 안됨.(input = getbalance보다 크면 안됨) 입력이 아예 -면 안됨
+		System.out.println("-------");
+		System.out.println("출금");
+		System.out.println("-------");
+		boolean runWithdraw = true;
+		int count3 = 0;
+		for (int i = 0; i < accountArray.length; i++) {
+			if (accountArray[i] != null) {
+				count3++;
+			}
+		}
+		if (count3 == 0) {
+			System.out.println("계좌정보가 없습니다. 계좌생성 먼저 해주세요.");
+			runWithdraw = false;
+		}
+		while (runWithdraw) {
+			int count4 = 0;
+			System.out.println("계좌번호 :");
+			String inputAccount = sc.nextLine();
+			System.out.println("출금액");
+			int withdraw = sc.nextInt();
+			sc.nextLine();
+
+			if (withdraw < 0) {
+				System.out.println("잘못된 금액입니다.(10~)");
+				continue;
+			}
+
+			for (int i = 0; i < accountArray.length; i++) {
+				if (accountArray[i] != null && inputAccount.equals(accountArray[i].getAccount())) {
+					// return값은 0과 1밖에없다. input에서 계좌중복, deposit에서 계좌가 아예없을때를 제어해줬다.
+					if ((accountArray[i].getBalance() - withdraw) < Account.MIN_BALANCE) {
+						System.out.println("출금불가. 잔액을 확인하세요.");
+						count4++;
+						break;
+					} else {
+						accountArray[i].setBalance(accountArray[i].getBalance() - withdraw);
+						System.out.println(accountArray[i].getBalance());
+						System.out.println("출금이 성공되었습니다.");
+						count4++;
+						break;
+					}
+				}
+
+			}
+			if (count4 == 0) {
+				System.out.println("계좌번호 오류입니다. 다시 입력해주세요.");
+				continue;
+			}
+			break;
+
+		}
+	}
+
 }
