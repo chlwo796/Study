fe41b369122a4bf133e913fc7f9170dbd173695c
chlwo796 <chlwo796@gmail.com>
2023-02-16 13:01:58 +0900

    230216 class

---
+++ java_study2/javaHigh05/src/javaHigh05/example03/ComparableEx01.java
@@ -0,0 +1,46 @@
+package javaHigh05.example03;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class ComparableEx01 {
+	public static void main(String[] args) {
+		TreeSet<Person> treeSet1 = new TreeSet<Person>();
+		// 사용자 정의 객체를 비교하지 않으면 ClassCastException 발생
+//		treeSet1.add(new Person("홍길동", 45));
+//		treeSet1.add(new Person("김자바", 25));
+//		treeSet1.add(new Person("박지원", 11));
+//		treeSet1.add(new Person("박지원", 19));
+//		treeSet1.add(new Person("박지원", 18));
+//		treeSet1.add(new Person("박지원3", 15));
+//		treeSet1.add(new Person("박지원4", 17));
+//		treeSet1.add(new Person("박지원5", 11));
+//
+//		Iterator<Person> iterator = treeSet1.iterator();
+//
+//		while (iterator.hasNext()) {
+//			Person person = iterator.next();
+//			System.out.println(person.getName() + " " + person.getAge());
+//
+//		}
+//		System.out.println();
+//		for (Person p : treeSet1) {
+//			System.out.println(p.getName() + " " + p.getAge());
+//		}
+
+		TreeSet<Person> treeSet2 = new TreeSet<Person>();
+		treeSet2.add(new Person("홍길동", 45, 1.4));
+		treeSet2.add(new Person("김자바", 25, 2.2));
+		treeSet2.add(new Person("박지원", 11, 5.4));
+		treeSet2.add(new Person("박지원", 19, 2.7));
+		treeSet2.add(new Person("박지원", 18, 3.2));
+		treeSet2.add(new Person("박지원3", 15, 6.2));
+		treeSet2.add(new Person("박지원4", 17, 1.3));
+		treeSet2.add(new Person("박지원5", 11, 2.6));
+		System.out.println();
+		for (Person p : treeSet2) {
+			System.out.println(p.getName() + " " + p.getAge() + " " + p.getScore());
+		}
+
+	}
+}
--- markdown/day33/day33.md
+++ markdown/day33/day33.md
@@ -1,4 +1,4 @@
-2023년 2월 16일 수요일
+2023년 2월 16일 목요일
 
 ---
 
@@ -6,10 +6,41 @@
 
 ### 1. remind
 
+- Tree 구조(Left-Root-Right)
+
+  ```text
+  4 7 0 8 9 2
+
+        4
+       / \
+      0   7
+       \   \
+        2   8
+             \
+              9
+  ```
+
+- Tree 장점 : 읽기만 해도 정렬이 되며, 검색 속도가 빠르다.
+- Tree 구조에서는 중복데이터를 허용하지 않기때문에 TreeSet,TreeMap으로 두가지가 있다.
+
 ---
 
 ### 2. java
 
+- TreeSet Method
+  - `Iterator<E> iterator()` : 오름차순 정렬구조
+  - `Iterator<E> descendingIterator()` : 내림차순 정렬구조
+  - `NavigableSet<E> descendingSet()` :
+- TreeMap method
+- Comparable, Comparator
+  - TreeSet에 저장되는 객체와 TreeMap에 저장되는 키 객체는 저장과 동시에 오름차순으로 정렬되는데, 어떤 객체든 정렬될 수 있는 것은 아니고 객체가 `Comparable` 인터페이스를 구현하고 있어야 가능하다.
+  - `Integer`, `Double`, `String` 타입은 기본적으로 `Comparable`을 구현하고 있기 때문에 상관이 없지만, 사용자 정의 객체를 저장할 때에는 반드시 `interface Comparable`을 구현하고 있어야 한다.
+- 사용자 정의 정렬(Comparable, Comparator)
+- Comparator : 데이터에 접근불가한 경우 별도의 비교하는 class를 생성하여 비교한다.
+
 ---
 
 ### 3. memo
+
+- [개발자 소양 5가지](https://yozm.wishket.com/magazine/detail/1896/)
+- 사용자 정의 정렬과 중복데이터 처리 합쳐보기
